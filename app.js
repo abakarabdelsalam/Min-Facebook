0564951604a1ce2da9176aeae35d293036521f5b
abakarabdelsalam <abdelsalamabakar3@gmail.com>
2021-04-02 12:26:18 +0200

    the first page Hello world

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const index = require('./routes');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(morgan('short'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(index);
+
+app.listen(3000);
